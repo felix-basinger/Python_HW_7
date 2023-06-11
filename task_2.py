462e1aacc1bb63c9cdf49695861bb65d3f3fc8dc
Basinger <fel_simon@mail.ru>
2023-06-12 01:26:58 +0300

    Added new task 2

--- task_2.py
+++ task_2.py
@@ -6,7 +6,7 @@
 # ровно два аргумента, как, например, у операции умножения.
 
 
-def print_operation_table(operation, num_rows=7, num_columns=7):
+def print_operation_table(operation, num_rows=6, num_columns=6):
     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in a:
         print(*[f"{x:>3}" for x in i])
